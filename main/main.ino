d936153a099e7abf37259478dccb48ea6f7fba64
Jan Rosa <102300602+jrosa02@users.noreply.github.com>
2024-04-06 17:22:02 +0200

    new_beginning

---
+++ main/main.ino
@@ -0,0 +1,15 @@
+#include <ACROBOTIC_SSD1306.h>
+#include <DHT11.h>
+#include <HMC5883L.h>
+#include <HCSR04.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
