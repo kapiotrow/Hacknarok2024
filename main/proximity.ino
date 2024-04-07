785d8dc7c0c5b7ac7114e5f18d62808314775031
Karolina Piotrowska <kapiotrow@student.agh.edu.pl>
2024-04-07 06:03:56 +0200

    fixed snow cover height calculations

--- main/proximity.ino
+++ main/proximity.ino
@@ -1,11 +1,11 @@
 #include <HCSR04.h>
 
-float base_height = 0; //we should get it from the encoder
+float base_height = 200;
 
 UltraSonicDistanceSensor distanceSensor(11, 12);
 
 void get_snow_cover_height() {
   float distance = distanceSensor.measureDistanceCm();
-  height = distance - base_height;
+  height = base_height - distance;
   return;
 }
