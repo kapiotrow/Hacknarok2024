e6472fe92a7303470ee84ec27b02c6b7cd2a5e23
StecBartlomiej <stecb@student.agh.edu.pl>
2024-04-07 06:23:56 +0200

    added server

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,25 @@
 #include <HCSR04.h>
 #include <QMC5883LCompass.h>
+#include <WiFi.h>
+
 
 QMC5883LCompass compass;
 int8_t temperature;
 int8_t humidity;
 int8_t  height;
 bool alarm;
+
+WiFiServer server(80);
+WiFiMulti multi;
+WiFiClient client;
+
+
+const char ssid[] = "KPT-Conference";
+const char password[] = "E2ue6Tm&";
+
+const char host[] = "172.98.2.183";
+const uint16_t port = 5000;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,15 +28,41 @@ void setup() {
   temperature = 100;
   height      = 0;
   alarm       = false;
+  
+  multi.addAP(ssid, password);
+  for (uint8_t i = 0; i < 3; ++i)
+  {
+    if (multi.run() != WL_CONNECTED) {
+      Serial.println("Unable to connect to network, rebooting in 2 seconds...");
+      // delay(50);
+    }
+  }
+  Serial.println("WiFi connected");
+
 
 }
 
+bool send_msg = false;
 void loop() {
+
   // put your main code here, to run repeatedly:
   request_temperature(13);
   delay(18);
   get_temperature(13);
   alarm = revolutions();
   screen(temperature, humidity, height, alarm);
+
+  if (send_msg) {
+    if (!client.connect(host, port)) {
+      Serial.println("connection failed");
+      return;
+    }
+    send_avalanche_msg(client);
+
+    client.stop();
+  }
+
   delay(2000-18);
+
+
 }
---
+++ main/server.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+
+const char avalanche_message[] = "avalanche";
+
+
+char* wifi_send_read(const char *send_data, int length, WiFiClient &client)
+{
+  static char read_data[128] = "";
+  Serial.println("sending data to server");
+
+  if (client.connected()) {
+    int written = client.print(send_data);
+  }
+
+  // wait for data to be available
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      // Server doesnt send response
+      Serial.println(">>> Client Timeout !");
+      return nullptr;
+    }
+  }
+
+  Serial.println("receiving from remote server");
+  int bytes_to_read = client.available(); 
+
+  if (bytes_to_read > 128 - 1)
+    return nullptr;
+
+  int i = 0;
+  for (; i < bytes_to_read + 1; ++i) {
+    read_data[i] = static_cast<char>(client.read());  
+    // Serial.println(wifi_read_data[i]);    
+  }
+  // read_data[i+1] = '\0';
+  Serial.println(read_data);
+  
+  return read_data;
+}
+
+void send_avalanche_msg(WiFiClient &client)
+{
+  char *read = wifi_send_read(avalanche_message, sizeof(avalanche_message), client);
+  if (read == nullptr)
+  {
+    Serial.println("No data received");
+  }
+} 
+
